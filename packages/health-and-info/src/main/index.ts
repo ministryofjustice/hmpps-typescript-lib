a68240b82bd5ff52a26b1a1f74ed49990f671b30
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-20 13:21:22 +0100

    Add index.ts to limit module exports and simplify use

---
+++ packages/health-and-info/src/main/index.ts
@@ -0,0 +1,34 @@
+import HealthCheckMiddleware from './HealthCheckMiddleware'
+import HealthCheck from './HealthCheck'
+import EndpointComponent from './components/EndpointComponent'
+
+/**
+ * HealthCheckMiddleware class provides middleware functions for handling
+ * health and deployment information endpoints in an Express.js application.
+ *
+ * Example:
+ *
+ * ```typescript
+ * const healthMiddleware = new HealthCheckMiddleware(options);
+ * app.get('/health', healthMiddleware.health);
+ * app.get('/info', healthMiddleware.info);
+ * app.get('/ping', healthMiddleware.ping);
+ * ```
+ */
+export function healthCheckMiddleware(...args: ConstructorParameters<typeof HealthCheckMiddleware>) {
+  return new HealthCheckMiddleware(...args)
+}
+
+export function healthCheck(...args: ConstructorParameters<typeof HealthCheck>) {
+  return new HealthCheck(...args)
+}
+
+/**
+ * EndpointComponent class implements the HealthComponent interface.
+ * It checks the health status of an external service by sending HTTP requests to a specified endpoint.
+ */
+export function endpointComponent(...args: ConstructorParameters<typeof EndpointComponent>) {
+  return new EndpointComponent(...args)
+}
+
+export type { HealthComponent, ComponentHealthResult } from './types/HealthComponent'
