d208b0c956df808e562ddf6e2b5282758e1fd0bd
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2024-10-15 15:36:49 +0100

    eslint-config-hmpps initial commit

---
+++ packages/eslint-config-hmpps/index.js
@@ -0,0 +1,18 @@
+const { rules: bestPracticesRules } = require('./rules/best-practices')
+const { rules: errorsRules } = require('./rules/errors')
+const { rules: nodeRules } = require('./rules/node')
+const { rules: styleRules } = require('./rules/style')
+const { rules: variablesRules } = require('./rules/variables')
+const { rules: es6Rules } = require('./rules/es6')
+const { rules: importsRules } = require('./rules/imports')
+const { rules: strictRules } = require('./rules/strict')
+
+const rules = Object.assign({}, bestPracticesRules, errorsRules, nodeRules, styleRules, variablesRules, es6Rules, importsRules, strictRules)
+
+module.exports = {
+  rules,
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+};
