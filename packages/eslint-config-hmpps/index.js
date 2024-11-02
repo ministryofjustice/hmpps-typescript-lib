ec454f241fef15aacffca45c06ca607e04470271
Igor Ushkarev <ushkarev@gmail.com>
2024-11-02 12:24:15 +0000

    Apply `npm run lint-fix` to eslint-config-hmpps package

--- packages/eslint-config-hmpps/index.js
+++ packages/eslint-config-hmpps/index.js
@@ -1,14 +1,14 @@
-const _import = require("eslint-plugin-import")
-const noOnlyTests = require("eslint-plugin-no-only-tests")
+const _import = require('eslint-plugin-import')
+const noOnlyTests = require('eslint-plugin-no-only-tests')
 
-const { fixupPluginRules } = require("@eslint/compat")
+const { fixupPluginRules } = require('@eslint/compat')
 
-const globals = require("globals")
-const typescriptEslint = require("@typescript-eslint/eslint-plugin")
-const tsParser = require("@typescript-eslint/parser")
-const js = require("@eslint/js")
+const globals = require('globals')
+const typescriptEslint = require('@typescript-eslint/eslint-plugin')
+const tsParser = require('@typescript-eslint/parser')
+const js = require('@eslint/js')
 
-const { FlatCompat } = require("@eslint/eslintrc")
+const { FlatCompat } = require('@eslint/eslintrc')
 
 const { rules: bestPracticesRules } = require('./airBnbRules/best-practices')
 const { rules: errorsRules } = require('./airBnbRules/errors')
@@ -19,20 +19,26 @@ const { rules: es6Rules } = require('./airBnbRules/es6')
 const { rules: importsRules } = require('./airBnbRules/imports')
 const { rules: strictRules } = require('./airBnbRules/strict')
 
-const airBnbRules = Object.assign({}, bestPracticesRules, errorsRules, nodeRules, styleRules, variablesRules, es6Rules, importsRules, strictRules)
+const airBnbRules = {
+  ...bestPracticesRules,
+  ...errorsRules,
+  ...nodeRules,
+  ...styleRules,
+  ...variablesRules,
+  ...es6Rules,
+  ...importsRules,
+  ...strictRules,
+}
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
+  allConfig: js.configs.all,
 })
 
 module.exports = [
   {
-    ignores: [
-      "**/node_modules",
-      "dist/",
-    ],
+    ignores: ['**/node_modules', 'dist/'],
     linterOptions: {
       reportUnusedDisableDirectives: true,
     },
@@ -40,11 +46,11 @@ module.exports = [
   {
     rules: airBnbRules,
   },
-  ...compat.extends("plugin:prettier/recommended"),
+  ...compat.extends('plugin:prettier/recommended'),
   {
     plugins: {
       import: fixupPluginRules(_import),
-      "no-only-tests": noOnlyTests,
+      'no-only-tests': noOnlyTests,
     },
     languageOptions: {
       globals: {
@@ -54,94 +60,118 @@ module.exports = [
       },
     },
     settings: {
-      "import/parsers": {
-        "@typescript-eslint/parser": [".ts", ".tsx"],
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
       },
-      "import/resolver": {
+      'import/resolver': {
         typescript: {
           alwaysTryTypes: true,
         },
         node: {
-          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
         },
       },
     },
     rules: {
-      "no-unused-vars": [1, {
-        argsIgnorePattern: "res|next|^err|_",
-        ignoreRestSiblings: true,
-      }],
-      "no-use-before-define": 0,
+      'no-unused-vars': [
+        1,
+        {
+          argsIgnorePattern: 'res|next|^err|_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'no-use-before-define': 0,
       semi: 0,
-      "import/no-named-as-default-member": 0,
-      "import/no-unresolved": "error",
-      "import/extensions": ["error", "ignorePackages", {
-        js: "never",
-        mjs: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
-      }],
-      "comma-dangle": ["error", "always-multiline"],
-      "import/no-extraneous-dependencies": ["error", {
-        devDependencies: [
-          "**/*.test.js",
-          "**/*.test.ts",
-          "**/test/**",
-          "**/testutils/**",
-          "cypress.config.ts",
-          "esbuild/**/*",
-        ],
-      }],
-      "no-only-tests/no-only-tests": "error",
-      "prettier/prettier": ["error", {
-        trailingComma: "all",
-        singleQuote: true,
-        printWidth: 120,
-        semi: false,
-      }],
-      "no-empty-function": ["error", {
-        allow: ["constructors", "arrowFunctions"],
-      }],
+      'import/no-named-as-default-member': 0,
+      'import/no-unresolved': 'error',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          mjs: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      'comma-dangle': ['error', 'always-multiline'],
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: [
+            '**/*.test.js',
+            '**/*.test.ts',
+            '**/test/**',
+            '**/testutils/**',
+            'cypress.config.ts',
+            'esbuild/**/*',
+          ],
+        },
+      ],
+      'no-only-tests/no-only-tests': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          trailingComma: 'all',
+          singleQuote: true,
+          printWidth: 120,
+          semi: false,
+        },
+      ],
+      'no-empty-function': [
+        'error',
+        {
+          allow: ['constructors', 'arrowFunctions'],
+        },
+      ],
     },
   },
-  ...compat.extends(
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ).map(config => ({
-    ...config,
-    files: ["**/*.ts"],
-    ignores: ["**/*.js"],
-  })),
+  ...compat
+    .extends(
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    )
+    .map(config => ({
+      ...config,
+      files: ['**/*.ts'],
+      ignores: ['**/*.js'],
+    })),
   {
-    files: ["**/*.ts"],
-    ignores: ["**/*.js"],
+    files: ['**/*.ts'],
+    ignores: ['**/*.js'],
     plugins: {
-      "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': typescriptEslint,
     },
     languageOptions: {
       parser: tsParser,
     },
     rules: {
-      "no-shadow": "off",
-      "@typescript-eslint/no-shadow": ["error"],
-      "@typescript-eslint/no-use-before-define": 0,
-      "class-methods-use-this": 0,
-      "no-useless-constructor": 0,
-      "@typescript-eslint/no-unused-vars": [1, {
-        argsIgnorePattern: "res|next|^err|_",
-        ignoreRestSiblings: true,
-      }],
-      "@typescript-eslint/semi": 0,
-      "import/no-unresolved": "error",
-      "import/no-named-as-default-member": 0,
-      "prettier/prettier": ["error", {
-        trailingComma: "all",
-        singleQuote: true,
-        printWidth: 120,
-        semi: false,
-      }],
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': ['error'],
+      '@typescript-eslint/no-use-before-define': 0,
+      'class-methods-use-this': 0,
+      'no-useless-constructor': 0,
+      '@typescript-eslint/no-unused-vars': [
+        1,
+        {
+          argsIgnorePattern: 'res|next|^err|_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/semi': 0,
+      'import/no-unresolved': 'error',
+      'import/no-named-as-default-member': 0,
+      'prettier/prettier': [
+        'error',
+        {
+          trailingComma: 'all',
+          singleQuote: true,
+          printWidth: 120,
+          semi: false,
+        },
+      ],
     },
-  }
+  },
 ]
