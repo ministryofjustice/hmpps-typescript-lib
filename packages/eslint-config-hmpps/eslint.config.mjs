f24f65f2f7cf63d4ad815301c825d6075547d16c
Igor Ushkarev <ushkarev@gmail.com>
2024-11-02 12:21:06 +0000

    Add linting to the eslint-config-hmpps package and rename rules folder to make it clear it only houses Airbnb rule sets More dogfooding, except in Airbnb folder as those files are taken as-is

---
+++ packages/eslint-config-hmpps/eslint.config.mjs
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/extensions
+import hmppsConfig from './index.js'
+
+export default [
+  // ignoring linting within Airbnb rules since they are vendored as-is
+  { ignores: ['airBnbRules/'] },
+  ...hmppsConfig,
+]
