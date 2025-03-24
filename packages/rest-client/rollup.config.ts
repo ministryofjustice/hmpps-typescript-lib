ce91867f52626dafdae954ea0b7df7add3abe56c
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-03-24 19:32:12 +0000

    Hack to make multi-self-referential workspace work

--- packages/rest-client/rollup.config.ts
+++ packages/rest-client/rollup.config.ts
@@ -1,10 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import path from 'path'
 import typescript from '@rollup/plugin-typescript'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { dts } from 'rollup-plugin-dts'
+import copy from 'rollup-plugin-copy'
+import pkg from './package.json'
 /* eslint-enable import/no-extraneous-dependencies */
 
-import pkg from './package.json'
+const outputDir = path.resolve(__dirname, '../../dist', pkg.name)
 
 export default [
   {
@@ -13,8 +16,35 @@ export default [
       { file: pkg.main, format: 'cjs', sourcemap: true },
       { file: pkg.module, format: 'esm', sourcemap: true },
     ],
-    plugins: [nodeResolve({ preferBuiltins: true }), typescript({ tsconfig: './tsconfig.json', noEmitOnError: true })],
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+    plugins: [
+      nodeResolve({ preferBuiltins: true }),
+      typescript({
+        tsconfig: './tsconfig.json',
+        noEmitOnError: true,
+      }),
+      copy({
+        targets: [
+          {
+            src: 'package.json',
+            dest: outputDir,
+            transform: () => {
+              return JSON.stringify(
+                {
+                  ...pkg,
+                  name: `@ministryofjustice/${pkg.name}`,
+                  devDependencies: {},
+                },
+                null,
+                2,
+              )
+            },
+          },
+          { src: '*.md', dest: outputDir },
+          { src: 'dist', dest: outputDir },
+        ],
+      }),
+    ],
   },
   {
     input: 'dist/main/index.d.ts',
