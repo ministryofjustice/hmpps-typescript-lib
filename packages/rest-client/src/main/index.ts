de09a035499a3a23a4aef4be1b00aac00d341f57
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2025-04-08 18:03:15 +0100

    Fix exporting AgentConfig class (#64)

--- packages/rest-client/src/main/index.ts
+++ packages/rest-client/src/main/index.ts
@@ -2,6 +2,8 @@ export { default as RestClient } from './RestClient'
 export { default as asUser } from './helpers/asUser'
 export { default as asSystem } from './helpers/asSystem'
 
-export type { ApiConfig, AgentConfig } from './types/ApiConfig'
+export type { ApiConfig } from './types/ApiConfig'
+export { AgentConfig } from './types/ApiConfig'
+
 export type { SanitisedError } from './types/Errors'
 export type { AuthenticationClient } from './types/AuthenticationClient'
