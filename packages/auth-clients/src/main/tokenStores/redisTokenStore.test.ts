d932bb3cca1fb15ed6a6f5782498b9008b6e4676
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-12-04 15:43:09 +0000

    RR-2125 - Allow setting of a custom prefix on RedisTokenStore (#119)
    * RR-2125 - Allow setting of a custom prefix on RedisTokenStore
    
    * RR-2125 - Bumped version in package.json

--- packages/auth-clients/src/main/tokenStores/redisTokenStore.test.ts
+++ packages/auth-clients/src/main/tokenStores/redisTokenStore.test.ts
@@ -12,45 +12,67 @@ const redisClient = {
 describe('tokenStore', () => {
   let tokenStore: TokenStore
 
-  beforeEach(() => {
-    tokenStore = new TokenStore(redisClient as unknown as RedisClient)
-  })
-
   afterEach(() => {
     jest.resetAllMocks()
   })
 
-  describe('get token', () => {
+  describe('Token Store with default prefix', () => {
+    beforeEach(() => {
+      tokenStore = new TokenStore(redisClient as unknown as RedisClient)
+    })
+
+    describe('get token', () => {
+      it('Can retrieve token', async () => {
+        redisClient.get.mockResolvedValue('token-1')
+
+        await expect(tokenStore.getToken('user-1')).resolves.toBe('token-1')
+
+        expect(redisClient.get).toHaveBeenCalledWith('systemToken:user-1')
+      })
+
+      it('Connects when no connection calling getToken', async () => {
+        ;(redisClient as unknown as Record<string, boolean>).isOpen = false
+
+        await tokenStore.getToken('user-1')
+
+        expect(redisClient.connect).toHaveBeenCalledWith()
+      })
+    })
+
+    describe('set token', () => {
+      it('Can set token', async () => {
+        await tokenStore.setToken('user-1', 'token-1', 10)
+
+        expect(redisClient.set).toHaveBeenCalledWith('systemToken:user-1', 'token-1', { EX: 10 })
+      })
+
+      it('Connects when no connection calling set token', async () => {
+        ;(redisClient as unknown as Record<string, boolean>).isOpen = false
+
+        await tokenStore.setToken('user-1', 'token-1', 10)
+
+        expect(redisClient.connect).toHaveBeenCalledWith()
+      })
+    })
+  })
+
+  describe('Token Store with custom prefix', () => {
+    beforeEach(() => {
+      tokenStore = new TokenStore(redisClient as unknown as RedisClient, 'customTokens')
+    })
+
     it('Can retrieve token', async () => {
       redisClient.get.mockResolvedValue('token-1')
 
       await expect(tokenStore.getToken('user-1')).resolves.toBe('token-1')
 
-      expect(redisClient.get).toHaveBeenCalledWith('systemToken:user-1')
+      expect(redisClient.get).toHaveBeenCalledWith('customTokens:user-1')
     })
 
-    it('Connects when no connection calling getToken', async () => {
-      ;(redisClient as unknown as Record<string, boolean>).isOpen = false
-
-      await tokenStore.getToken('user-1')
-
-      expect(redisClient.connect).toHaveBeenCalledWith()
-    })
-  })
-
-  describe('set token', () => {
     it('Can set token', async () => {
       await tokenStore.setToken('user-1', 'token-1', 10)
 
-      expect(redisClient.set).toHaveBeenCalledWith('systemToken:user-1', 'token-1', { EX: 10 })
-    })
-
-    it('Connects when no connection calling set token', async () => {
-      ;(redisClient as unknown as Record<string, boolean>).isOpen = false
-
-      await tokenStore.setToken('user-1', 'token-1', 10)
-
-      expect(redisClient.connect).toHaveBeenCalledWith()
+      expect(redisClient.set).toHaveBeenCalledWith('customTokens:user-1', 'token-1', { EX: 10 })
     })
   })
 })
