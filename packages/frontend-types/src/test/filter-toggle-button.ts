d1a722ea96c419bb41eb56b74de0795d6e001f3b
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2026-04-02 17:24:24 +0100

    allow the component config class to have objects

---
+++ packages/frontend-types/src/test/filter-toggle-button.ts
@@ -0,0 +1,24 @@
+import { FilterToggleButton } from '@ministryofjustice/frontend'
+
+export default function filterToggleButton() {
+  // eslint-disable-next-line no-new
+  new FilterToggleButton(document.querySelector('[data-module="moj-filter"]') as HTMLElement, {
+    bigModeMediaQuery: '(min-width: 48.063em)',
+    startHidden: false,
+    toggleButton: {
+      showText: 'Show filters',
+      hideText: 'Hide filters',
+      classes: 'govuk-button--secondary',
+    },
+    toggleButtonContainer: {
+      selector: '.moj-action-bar__filter',
+    },
+    closeButton: {
+      text: 'Close',
+      classes: 'moj-filter__close',
+    },
+    closeButtonContainer: {
+      selector: '.moj-filter__header-action',
+    },
+  })
+}
